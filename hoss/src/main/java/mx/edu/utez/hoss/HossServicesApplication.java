ea01e69d02df4bcac6e39082e9e5833d863ce93d
SaraMCG <84095824+SaraMCG@users.noreply.github.com>
2022-09-30 22:02:41 -0500

    first commit

---
+++ hoss/src/main/java/mx/edu/utez/hoss/HossServicesApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.hoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HossServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HossServicesApplication.class, args);
+	}
+
+}
---
+++ hoss/src/test/java/mx/edu/utez/hoss/HossServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.hoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HossServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
